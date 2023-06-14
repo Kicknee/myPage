8a07b397235f30cf06682f06bf5621b68a745a90
Kicknee <daniszewski21@gmail.com>
2023-06-15 01:42:47 +0200

    404 page

---
+++ src/pages/404.js
@@ -0,0 +1,48 @@
+import React from "react";
+import Container from "../components/Container";
+import { navigate } from "gatsby";
+import SEO from "../components/SEO";
+
+const Error = () => {
+  setTimeout(() => {
+    navigate("/");
+  }, 5000);
+
+  return (
+    <Container>
+      <SEO title="404" />
+      <main className="error-404">
+        <div className="rainyCloudBox">
+          <div className="message-404">
+            <span>#</span>404
+          </div>
+          <div className="cloud"></div>
+          <div className="rain">
+            <span style={{ "--i": "11" }}></span>
+            <span style={{ "--i": "12" }}></span>
+            <span style={{ "--i": "10" }}></span>
+            <span style={{ "--i": "14" }}></span>
+            <span style={{ "--i": "18" }}></span>
+            <span style={{ "--i": "16" }}></span>
+            <span style={{ "--i": "19" }}></span>
+            <span style={{ "--i": "20" }}></span>
+            <span style={{ "--i": "19" }}></span>
+            <span style={{ "--i": "10" }}></span>
+            <span style={{ "--i": "16" }}></span>
+            <span style={{ "--i": "14" }}></span>
+            <span style={{ "--i": "18" }}></span>
+            <span style={{ "--i": "11" }}></span>
+            <span style={{ "--i": "13" }}></span>
+            <span style={{ "--i": "15" }}></span>
+            <span style={{ "--i": "12" }}></span>
+            <span style={{ "--i": "17" }}></span>
+            <span style={{ "--i": "13" }}></span>
+            <span style={{ "--i": "15" }}></span>
+          </div>
+        </div>
+      </main>
+    </Container>
+  );
+};
+
+export default Error;
