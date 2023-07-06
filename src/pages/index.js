6ae72336e8297e408a98a8a313a85666bd4527eb
Kicknee <daniszewski21@gmail.com>
2023-07-06 12:58:27 +0200

    fixes for contact form - added required attribute

--- src/pages/index.js
+++ src/pages/index.js
@@ -154,11 +154,11 @@ const IndexPage = (props) => {
         <section className="contact" id="contact">
           <form action="https://formspree.io/f/mknadrqp" method="POST">
             <label htmlFor="name">Imię i nazwisko:</label>
-            <input type="text" name="name" id="name" />
+            <input type="text" name="name" id="name" required />
             <label htmlFor="email">Adres email:</label>
-            <input type="email" name="email" id="email" />
+            <input type="email" name="email" id="email" required />
             <label htmlFor="message">Treść wiadomości:</label>
-            <textarea type="textArea" name="message" id="message" />
+            <textarea type="textArea" name="message" id="message" required />
             <button type="submit" className="submit">
               WYŚLIJ
             </button>
