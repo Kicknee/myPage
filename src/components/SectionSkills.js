a927698bc1bb015a2dacbff2090f27cab9b74994
Kicknee <daniszewski21@gmail.com>
2023-09-26 18:07:08 +0200

    updated skills section

--- src/components/SectionSkills.js
+++ src/components/SectionSkills.js
@@ -3,19 +3,19 @@ import React from "react";
 function SectionSkills() {
   return (
     <section className="skills" id="skills">
-      <div className="skill color">HTML</div>
-      <div className="skill">EXPRESS.JS</div>
-      <div className="skill">CSS</div>
-      <div className="skill color">PHOTOSHOP</div>
-      <div className="skill color">JAVA SCRIPT</div>
-      <div className="skill">NODE.JS / NPM</div>
+      <div className="skill">HTML</div>
       <div className="skill">SASS</div>
-      <div className="skill color">MongoDB</div>
-      <div className="skill color">REACT.JS</div>
+      <div className="skill">CSS</div>
       <div className="skill">GIT</div>
+      <div className="skill">JAVA SCRIPT</div>
+      <div className="skill">MongoDB</div>
       <div className="skill">TypeScript</div>
-      <div className="skill color">DOCKER</div>
-      <div className="skill color">GATSBY</div>
+      <div className="skill">DOCKER</div>
+      <div className="skill">REACT.JS</div>
+      <div className="skill">EXPRESS.JS</div>
+      <div className="skill">Redux</div>
+      <div className="skill">PHOTOSHOP</div>
+      <div className="skill">GATSBY</div>
     </section>
   );
 }
