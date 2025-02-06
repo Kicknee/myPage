465c29533b8f9e628d56ed37ba36819c6e2b1af4
Kicknee <daniszewski21@gmail.com>
2025-02-06 23:53:11 +0100

    update skills section

--- src/components/SectionSkills.js
+++ src/components/SectionSkills.js
@@ -3,19 +3,20 @@ import React from "react";
 function SectionSkills() {
   return (
     <section className="skills" id="skills">
-      <div className="skill">HTML</div>
-      <div className="skill">SASS</div>
-      <div className="skill">CSS</div>
-      <div className="skill">GIT</div>
-      <div className="skill">JAVA SCRIPT</div>
-      <div className="skill">SQL (MySQL)</div>
-      <div className="skill">TypeScript</div>
-      <div className="skill">DOCKER</div>
-      <div className="skill">REACT.JS</div>
-      <div className="skill">EXPRESS.JS</div>
+      <div className="skill">HTML5</div>
       <div className="skill">Redux</div>
-      <div className="skill">PHOTOSHOP</div>
-      <div className="skill">GATSBY</div>
+      <div className="skill">CSS3</div>
+      <div className="skill">Gatsby</div>
+      <div className="skill">JavaScript</div>
+      <div className="skill">Next.js</div>
+      <div className="skill">SASS</div>
+      <div className="skill">Express.JS</div>
+      <div className="skill">TypeScript</div>
+      <div className="skill">MongoDB</div>
+      <div className="skill">React.js</div>
+      <div className="skill">GIT</div>
+      <div className="skill">React Native</div>
+      <div className="skill">Photoshop</div>
     </section>
   );
 }
