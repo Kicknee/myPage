cd869a155db95ceb144cda7571eabd0b4858b3a4
Kicknee <daniszewski21@gmail.com>
2025-03-07 20:20:58 +0100

    change for front end developer

--- src/components/SectionPresentation.js
+++ src/components/SectionPresentation.js
@@ -4,11 +4,11 @@ const SectionPresentation = () => {
   return (
     <section className="presentation" id="presentation">
       <span className="greeting">
-        <span className="czesc">CZEŚĆ</span> 🖖, NAZYWAM SIĘ
+        <span>CZEŚĆ</span> 🖖, NAZYWAM SIĘ
       </span>
       <span className="name">Paweł Daniszewski</span>
       <span className="occupation">DEVELOPER</span>
-      <span className="more-info">Jestem front-end developerem z Warszawy</span>
+      <span className="more-info">Jestem front end developerem z Warszawy</span>
     </section>
   );
 };
