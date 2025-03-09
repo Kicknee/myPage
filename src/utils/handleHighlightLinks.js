77bc0d8002673231ea14f22b92c06003cccd907e
Kicknee <daniszewski21@gmail.com>
2025-03-10 00:00:11 +0100

    declare a function in module

--- src/utils/handleHighlightLinks.js
+++ src/utils/handleHighlightLinks.js
@@ -1,26 +1,27 @@
-const sections = document.querySelectorAll("section:not(.presentation)");
+export function handleHighlightLinks() {
+  const sections = document.querySelectorAll("section:not(.presentation)");
 
-const sectionObserver = new IntersectionObserver(
-  (entries) => {
-    entries.forEach((entry) => {
-      const currentSection = document.querySelector(
-        `#${entry.target.className}-link`
-      );
-      if (entry.isIntersecting) {
-        currentSection.classList.add("active");
-        console.log(entry.target.id);
-      }
-      if (!entry.isIntersecting) {
-        currentSection.classList.remove("active");
-      }
-    });
-  },
-  {
-    root: document.querySelector(".container"),
-    threshold: 0.3,
-  }
-);
+  const sectionObserver = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        const currentSection = document.querySelector(
+          `#${entry.target.className}-link`
+        );
+        if (entry.isIntersecting) {
+          currentSection.classList.add("active");
+        }
+        if (!entry.isIntersecting) {
+          currentSection.classList.remove("active");
+        }
+      });
+    },
+    {
+      root: document.querySelector(".container"),
+      threshold: 0.3,
+    }
+  );
 
-sections.forEach((section) => {
-  sectionObserver.observe(section);
-});
+  sections.forEach((section) => {
+    sectionObserver.observe(section);
+  });
+}
