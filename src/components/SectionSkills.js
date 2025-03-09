d90c0173acc6dfa814524b8c9a5958d7fbacd16d
Kicknee <daniszewski21@gmail.com>
2025-03-10 00:26:53 +0100

    update skills section

--- src/components/SectionSkills.js
+++ src/components/SectionSkills.js
@@ -4,19 +4,22 @@ function SectionSkills() {
   return (
     <section className="skills" id="skills">
       <div className="skill">HTML5</div>
-      <div className="skill">Redux</div>
+      <div className="skill">Express.js</div>
       <div className="skill">CSS3</div>
-      <div className="skill">Bootstrap</div>
-      <div className="skill">JavaScript</div>
-      <div className="skill">Gatsby</div>
-      <div className="skill">SASS</div>
-      <div className="skill">Next.js</div>
-      <div className="skill">TypeScript</div>
-      <div className="skill">Express.JS</div>
-      <div className="skill">React.js</div>
       <div className="skill">MongoDB</div>
-      <div className="skill">React Native</div>
+      <div className="skill">SASS</div>
       <div className="skill">GIT</div>
+      <div className="skill">Tailwind</div>
+      <div className="skill">Redux</div>
+      <div className="skill">Bootstrap</div>
+      <div className="skill">Zustand</div>
+      <div className="skill">JavaScript</div>
+      <div className="skill">TypeScript</div>
+      <div className="skill">React.js</div>
+      <div className="skill">Next.js</div>
+      <div className="skill">React Native</div>
+      <div className="skill">Gatsby</div>
+      <div className="skill">jQuery</div>
     </section>
   );
 }
