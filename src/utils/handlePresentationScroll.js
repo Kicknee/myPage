1a54233212debe0a4ac727ac9d795c2b5f3c1ff8
Kicknee <daniszewski21@gmail.com>
2025-03-09 23:19:22 +0100

    add logic to highlight main link when needed

--- src/utils/handlePresentationScroll.js
+++ src/utils/handlePresentationScroll.js
@@ -1,15 +1,16 @@
 export const handlePresentationScroll = () => {
   const windowWidth = window.innerWidth;
 
-  if (windowWidth > 1000) {
-    const container = document.querySelector(".container");
-    const presentation = document.querySelector(".presentation");
-    const skills = document.querySelector(".skills");
-    const windowOffSet = Math.floor(window.innerHeight * 0.1 + 55);
-    const presentationOffSetTop = presentation.offsetTop;
-    const skillsOffSetTop = skills.offsetTop;
-    const scrollOffSetTop = container.scrollTop;
+  const container = document.querySelector(".container");
+  const presentation = document.querySelector(".presentation");
+  const skills = document.querySelector(".skills");
+  const presentationLink = document.querySelector("#presentation-link");
+  const windowOffSet = Math.floor(window.innerHeight * 0.1 + 55);
+  const presentationOffSetTop = presentation.offsetTop;
+  const skillsOffSetTop = skills.offsetTop;
+  const scrollOffSetTop = container.scrollTop;
 
+  if (windowWidth > 1000) {
     if (
       skillsOffSetTop >
       presentationOffSetTop + scrollOffSetTop - windowOffSet
@@ -18,5 +19,11 @@ export const handlePresentationScroll = () => {
     }
   } else {
     document.querySelector(".presentation").style.transform = `translateY(0px)`;
+  }
+
+  if (scrollOffSetTop < windowOffSet * 1.5) {
+    presentationLink.classList.add("active");
+  } else {
+    presentationLink.classList.remove("active");
   }
 };
