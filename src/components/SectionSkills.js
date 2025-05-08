c419f2576bae77f239c6fda5be102111fb680c08
Kicknee <daniszewski21@gmail.com>
2025-05-08 21:29:03 +0200

    change skills section structure to left and right to align child elements with subgrid

--- src/components/SectionSkills.js
+++ src/components/SectionSkills.js
@@ -3,23 +3,29 @@ import React from "react";
 function SectionSkills() {
   return (
     <section className="skills" id="skills">
-      <div className="skill">HTML5</div>
-      <div className="skill">Express.js</div>
-      <div className="skill">CSS3</div>
-      <div className="skill">MongoDB</div>
-      <div className="skill">SASS</div>
-      <div className="skill">GIT</div>
-      <div className="skill">Tailwind</div>
-      <div className="skill">Redux</div>
-      <div className="skill">Bootstrap</div>
-      <div className="skill">Zustand</div>
-      <div className="skill">JavaScript</div>
-      <div className="skill">TypeScript</div>
-      <div className="skill">React.js</div>
-      <div className="skill">Next.js</div>
-      <div className="skill">React Native</div>
-      <div className="skill">Gatsby</div>
-      <div className="skill">jQuery</div>
+      <div className="left">
+        <div className="skill">HTML5</div>
+        <div className="skill">CSS3</div>
+        <div className="skill">SASS</div>
+        <div className="skill">Tailwind</div>
+        <div className="skill">Bootstrap</div>
+        <div className="skill">JavaScript</div>
+        <div className="skill">React.js</div>
+        <div className="skill">React Native</div>
+        <div className="skill">Vue.js</div>
+        <div className="skill">jQuery</div>
+      </div>
+      <div className="right">
+        <div className="skill">Express.js</div>
+        <div className="skill">MongoDB</div>
+        <div className="skill">Cypress</div>
+        <div className="skill">GIT</div>
+        <div className="skill">Redux</div>
+        <div className="skill">Zustand</div>
+        <div className="skill">TypeScript</div>
+        <div className="skill">Next.js</div>
+        <div className="skill">Gatsby</div>
+      </div>
     </section>
   );
 }
