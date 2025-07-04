8f1551428d662b86671cfc4bd4e3c830a1907295
Kicknee <daniszewski21@gmail.com>
2025-07-04 06:34:13 +0200

    fix scroll handle by referencing elements based on id

--- src/utils/handlePresentationScroll.js
+++ src/utils/handlePresentationScroll.js
@@ -1,15 +1,13 @@
 export const handlePresentationScroll = () => {
   const windowWidth = window.innerWidth;
-
-  const container = document.querySelector(".container");
-  const presentation = document.querySelector(".presentation");
-  const skills = document.querySelector(".skills");
-  const presentationLink = document.querySelector("#presentation-link");
+  const container = document.getElementById("container");
+  const presentation = document.getElementById("presentation");
+  const skills = document.getElementById("skills");
+  const presentationLink = document.getElementById("presentation-link");
   const windowOffSet = Math.floor(window.innerHeight * 0.1 + 55);
   const presentationOffSetTop = presentation.offsetTop;
   const skillsOffSetTop = skills.offsetTop;
   const scrollOffSetTop = container.scrollTop;
-
   if (windowWidth > 1000) {
     if (
       skillsOffSetTop >
@@ -18,9 +16,8 @@ export const handlePresentationScroll = () => {
       presentation.style.transform = `translateY(${scrollOffSetTop}px)`;
     }
   } else {
-    document.querySelector(".presentation").style.transform = `translateY(0px)`;
+    document.getElementById("presentation").style.transform = `translateY(0px)`;
   }
-
   if (scrollOffSetTop < windowOffSet * 1.5) {
     presentationLink.classList.add("active");
   } else {
