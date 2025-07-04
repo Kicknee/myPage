f0279db500ab40e72a42804d317e4fff037c72d1
Kicknee <daniszewski21@gmail.com>
2025-07-04 05:51:34 +0200

    add navbar styled component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,68 @@
 import React from "react";
 import { Link } from "gatsby";
+import styled from "styled-components";
 
+const StyledNavigation = styled.nav`
+  position: fixed;
+  top: 7vh;
+  left: 50vw;
+  width: 595px;
+  height: 55px;
+  z-index: 13;
+  display: flex;
+  justify-content: end;
+  padding: 0 20px;
+
+  &:before {
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    background-color: ${(props) => props.theme.color.primary};
+    z-index: -1;
+    backdrop-filter: blur(5px);
+  }
+  ul {
+    list-style: none;
+    width: 100%;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+  }
+  a {
+    text-decoration: none;
+    color: white;
+    font-size: 1.3rem;
+    &:hover {
+      color: #f68c22;
+    }
+    &.active {
+      color: #ff9933;
+    }
+  }
+
+  ${(props) => props.theme.mq.sm} {
+    padding: 0 5px;
+
+    a {
+      font-size: 1.1rem;
+    }
+  }
+  ${(props) => props.theme.mq.lg} {
+    width: 100%;
+    top: 10px;
+    left: 0;
+  }
+  ${(props) => props.theme.mq.xl} {
+    width: 500px;
+  }
+`;
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <ul className="nav-links">
+    <StyledNavigation>
+      <ul>
         <li>
           <Link to="/" id="presentation-link">
             Główna
@@ -26,7 +84,7 @@ const Navbar = () => {
           </Link>
         </li>
       </ul>
-    </nav>
+    </StyledNavigation>
   );
 };
 
