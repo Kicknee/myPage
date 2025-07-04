048f9c312d3af2ab20262ae56b58d4a740425a03
Kicknee <daniszewski21@gmail.com>
2025-07-04 05:45:21 +0200

    add global breakpoints to the theme

--- src/assets/styles/theme.js
+++ src/assets/styles/theme.js
@@ -7,4 +7,10 @@ export const theme = {
       inter: `"Inter", sans-serif"`,
     },
   },
+  mq: {
+    sm: `@media (max-width: 550px)`,
+    lg: `@media (max-width: 1000px)`,
+    xl: `@media (max-width: 1150px)`,
+    xxl: `@media (max-width: 1300px)`,
+  },
 };
