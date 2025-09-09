c93f4e00a010d9fa15cea67398b4e6cc026b201e
Kicknee <daniszewski21@gmail.com>
2025-09-09 23:55:45 +0200

    create context to share translations across the app

---
+++ src/context/TranslationContext.js
@@ -0,0 +1,41 @@
+import React, { createContext, useContext, useState } from "react";
+import en from "../../locales/en.json";
+import pl from "../../locales/pl.json";
+
+const TranslationContext = createContext();
+
+const translations = { en, pl };
+
+const TranslationProvider = ({ children }) => {
+  const [language, setLanguage] = useState("pl");
+
+  const t = (key) => {
+    const keys = key.split(".");
+    let current = translations[language];
+    for (let k of keys) {
+      if (current[k] === undefined) {
+        return key; // Return the key itself if translation is missing
+      }
+      current = current[k];
+    }
+    return current || key;
+  };
+
+  return (
+    <TranslationContext.Provider value={{ t, language, setLanguage }}>
+      {children}
+    </TranslationContext.Provider>
+  );
+};
+
+export { TranslationProvider };
+
+export const useTranslationContext = () => {
+  const context = useContext(TranslationContext);
+  if (!context) {
+    throw new Error(
+      "useTranslationContext must be used within a TranslationProvider"
+    );
+  }
+  return context;
+};
